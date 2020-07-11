316adb0d7fc1d65c9e6691061ebbe37d054005ae
Kevin <kevin@magicmonkey.org>
2020-07-11 11:19:21 +0100

    Temporarily prevent deployment as the RasPi is not running at the moment

--- make.sh
+++ make.sh
@@ -3,5 +3,5 @@
 set -xe
 
 CC=/opt/cross-pi-gcc/bin/arm-linux-gnueabihf-gcc CGO_ENABLED=1 GOARCH=arm GOARM=6 go build -o cnc_gamepad-raspi
-scp -i raspi.key cnc_gamepad-raspi pi@10.1.0.123:cnc/cnc_gamepad
+echo scp -i raspi.key cnc_gamepad-raspi pi@10.1.0.123:cnc/cnc_gamepad
 
